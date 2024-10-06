1b608e1946f3dedbd76d88f4710cf2250fd0184c
egormarik <egormarikpt@gmail.com>
2024-10-06 10:45:49 +0300

    edited string-formating-hw.py

---
+++ string-formating-hw.py
@@ -0,0 +1,8 @@
+
+numbers = [
+    1.23456, 12.1, 123.4567, 0.98765,
+    1234.5678, 56.789, 0.1, 3.14159
+]
+
+print("{:>15.4f} {:>15.4f} {:>15.4f} {:>15.4f}".format(numbers[0], numbers[1], numbers[2], numbers[3]))
+print("{:>15.4f} {:>15.4f} {:>15.4f} {:>15.4f}".format(numbers[4], numbers[5], numbers[6], numbers[7]))
